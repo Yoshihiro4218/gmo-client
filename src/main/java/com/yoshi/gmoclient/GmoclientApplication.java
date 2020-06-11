9c03cff4d04fd4afa1eaccdfe91eefe5bc33bfd7
yoshihiro <y.kadono4218@gmail.com>
2020-06-11 22:41:49 +0900

    first commit

---
+++ src/main/java/com/yoshi/gmoclient/GmoclientApplication.java
@@ -0,0 +1,13 @@
+package com.yoshi.gmoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmoclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmoclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoshi/gmoclient/GmoclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshi.gmoclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmoclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
