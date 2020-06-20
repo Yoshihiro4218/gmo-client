ee929dfef0e3b40485a0ff1c51550e964ede483b
yoshihiro <y.kadono4218@gmail.com>
2020-06-20 17:17:58 +0900

    email取る

--- src/main/java/com/yoshi/gmoclient/app/controller/HomeController.java
+++ src/main/java/com/yoshi/gmoclient/app/controller/HomeController.java
@@ -1,12 +1,25 @@
 package com.yoshi.gmoclient.app.controller;
 
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.springframework.security.oauth2.client.authentication.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.*;
 
 @Controller
+@AllArgsConstructor
+@Slf4j
 public class HomeController {
+    private final RestTemplate restTemplate;
+
     @GetMapping("/home")
-    public String home() {
+    public String home(OAuth2AuthenticationToken token,
+                       Model model) {
+        log.info("Token->{}", token);
+        model.addAttribute("email", token.getPrincipal()
+                                         .getAttributes().get("email"));
         return "pages/home";
     }
 }
